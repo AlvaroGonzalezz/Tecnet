6073e8c97678dd32c52ae61e7e4300ad6780ec27
LuisBarajas10 <luis.barajas.22isc@tecsanpedro.edu.mx>
2026-03-20 00:03:52 -0600

    Se termino la pagina de acerca de y se empezaron a terminar los dashboard

---
+++ portal/dashboard_docente.php
@@ -0,0 +1,458 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Docente - TecNet</title>
+  <link rel="shortcut icon" href="../dist/img/tecneticon.png" type="image/x-icon">
+  <link rel="stylesheet" href="plugins/fontawesome-free/css/all.min.css">
+  <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.13.1/font/bootstrap-icons.min.css">
+  <link rel="stylesheet" href="plugins/tempusdominus-bootstrap-4/css/tempusdominus-bootstrap-4.min.css">
+  <link rel="stylesheet" href="plugins/icheck-bootstrap/icheck-bootstrap.min.css">
+  <link rel="stylesheet" href="plugins/jqvmap/jqvmap.min.css">
+  <link rel="stylesheet" href="dist/css/adminlte.min.css">
+  <link rel="stylesheet" href="plugins/overlayScrollbars/css/OverlayScrollbars.min.css">
+  <link rel="stylesheet" href="plugins/daterangepicker/daterangepicker.css">
+  <link rel="stylesheet" href="plugins/summernote/summernote-bs4.min.css">
+</head>
+
+<body class="hold-transition sidebar-mini layout-fixed">
+  <div class="wrapper">
+
+    <!-- <div class="preloader flex-column justify-content-center align-items-center">
+    <img class="animation__shake" src="../dist/img/tecneticon.png" alt="AdminLTELogo" height="60" width="60">
+  </div> -->
+
+
+    <!-- Main Sidebar Container -->
+    <aside class="main-sidebar sidebar-dark-primary elevation-4">
+      <!-- Brand Logo -->
+      <a href="index3.html" class="brand-link">
+        <img src="../dist/img/tecneticon.png" alt="TecNet Logo" class="brand-image img-circle elevation-3"
+          style="opacity: .8">
+        <span class="brand-text font-weight-light" style="font-family: monospace;">TECNET</span>
+      </a>
+
+      <!-- Sidebar -->
+      <div class="sidebar">
+        <!-- Sidebar user panel (optional) -->
+        <div class="user-panel mt-3 pb-3 mb-3 d-flex">
+          <div class="image">
+            <img src="dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
+          </div>
+          <div class="info">
+            <a href="#" class="d-block">Administrador</a>
+          </div>
+        </div>
+
+
+
+        <!-- Sidebar Menu -->
+        <nav class="mt-2">
+          <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu"
+            data-accordion="false">
+            <!-- Add icons to the links using the .nav-icon class
+               with font-awesome or any other icon font library -->
+            <li class="nav-item menu-open">
+              <a href="#" class="nav-link active">
+                🏠
+                <p>
+                  Dashboard
+                </p>
+              </a>
+
+            <li class="nav-item">
+              <a href="#" class="nav-link">
+                📊
+                <p>
+                  Reportes
+                  <i class="right fas fa-angle-left"></i>
+                </p>
+              </a>
+                  <ul class="nav nav-treeview">
+                
+              
+                
+                <li class="nav-item">
+                  <a href="pages/charts/flot.html" class="nav-link">
+                    <i class="far fa-circle nav-icon"></i>
+                    <p>Académico</p>
+                  </a>
+                </li>
+              </ul>
+            </li>
+            <!-- <li class="nav-item">
+            <a href="#" class="nav-link">
+              <i class="nav-icon fas fa-edit"></i>
+              <p>
+                Forms
+                <i class="fas fa-angle-left right"></i>
+              </p>
+            </a>
+            <ul class="nav nav-treeview">
+              <li class="nav-item">
+                <a href="pages/forms/general.html" class="nav-link">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>General Elements</p>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a href="pages/forms/advanced.html" class="nav-link">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>Advanced Elements</p>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a href="pages/forms/editors.html" class="nav-link">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>Editors</p>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a href="pages/forms/validation.html" class="nav-link">
+                  <i class="far fa-circle nav-icon"></i>
+                  <p>Validation</p>
+                </a>
+              </li>
+            </ul>
+          </li> -->
+            <li class="nav-item">
+              <a href="#" class="nav-link">
+                🧾
+                <p>
+                  Gestión
+                  <i class="fas fa-angle-left right"></i>
+                </p>
+              </a>
+              <ul class="nav nav-treeview">
+                
+              
+                <li class="nav-item">
+                  <a href="pages/tables/data.html" class="nav-link">
+                    <i class="far fa-circle nav-icon"></i>
+                    <p>Estudiantes</p>
+                  </a>
+                </li>
+                <li class="nav-item">
+                  <a href="pages/tables/data.html" class="nav-link">
+                    <i class="far fa-circle nav-icon"></i>
+                    <p>Asignaturas</p>
+                  </a>
+                </li>
+               
+                <li class="nav-item">
+                 <a href="pages/charts/chartjs.html" class="nav-link">
+                    <i class="far fa-circle nav-icon"></i>
+                    <p>Carreras</p>
+                  </a>
+                </li>
+                <li class="nav-item">
+                  <a href="pages/tables/data.html" class="nav-link">
+                    <i class="far fa-circle nav-icon"></i>
+                    <p>Horarios</p>
+                  </a>
+                </li>
+              </ul>
+            </li>
+
+            <li class="nav-item">
+              <a href="pages/calendar.html" class="nav-link">
+                📅
+                <p>
+                  Calendario
+                </p>
+              </a>
+            </li>
+            <li class="nav-item">
+              <a href="pages/examples/profile.html" class="nav-link">
+                🧑
+                <p>Perfil</p>
+              </a>
+            </li>
+            <li class="nav-item">
+              <a href="pages/examples/profile.html" class="nav-link">
+                🔒
+                <p>Cerrar Sesión</p>
+              </a>
+            </li>
+          </ul>
+        </nav>
+      </div>
+    </aside>
+
+    <div class="content-wrapper">
+      <div class="content-header">
+        <div class="container-fluid">
+          <div class="row mb-2">
+            <div class="col-sm-6"><br>
+              <h1 class="m-0">¡Hola, moi!👋</h1>
+            </div><!-- /.col -->
+
+          </div><!-- /.row -->
+        </div><!-- /.container-fluid -->
+      </div>
+      <!-- /.content-header -->
+
+      <!-- Main content -->
+      <section class="content">
+        <div class="container-fluid">
+          <!-- Small boxes (Stat box) -->
+          <div class="row">
+            <div class="col-lg-3 col-6">
+              <!-- small box -->
+              <div class="small-box bg-info">
+                <div class="inner">
+                  <h3>1200</h3>
+
+                  <p>Alumnos</p>
+                </div>
+                <div class="icon">
+                  <i class="ion bi bi-mortarboard-fill"></i>
+                </div>
+                <a href="#" class="small-box-footer">Más Información <i
+                    class="fas fa-arrow-circle-right"></i></a>
+              </div>
+            </div>
+            <!-- ./col -->
+            <div class="col-lg-3 col-6">
+              <!-- small box -->
+              <div class="small-box bg-success">
+                <div class="inner">
+                  <h3>4</h3>
+
+                  <p>Carreras</p>
+                </div>
+                <div class="icon">
+                  <i class="ion bi bi-person-vcard-fill"></i>
+                </div>
+                <a href="#" class="small-box-footer">Más Información <i
+                    class="fas fa-arrow-circle-right"></i></a>
+              </div>
+            </div>
+            <!-- ./col -->
+            <div class="col-lg-3 col-6">
+              <!-- small box -->
+              <div class="small-box bg-warning">
+                <div class="inner">
+                  <h3>44</h3>
+
+                  <p>Docentes</p>
+                </div>
+                <div class="icon">
+                  <i class="ion bi bi-person-workspace"></i>
+                </div>
+                <a href="#" class="small-box-footer">Más Información <i
+                    class="fas fa-arrow-circle-right"></i></a>
+              </div>    
+            </div>
+            <!-- ./col -->
+            <div class="col-lg-3 col-6">
+              <!-- small box -->
+             
+                <!-- ./col -->
+            
+          <!-- /.row -->
+          <!-- Main row -->
+          <div class="row">
+            <!-- Left col -->
+          
+            <!-- /.Left col -->
+            <!-- right col (We are only adding the ID to make the widgets sortable)-->
+            <section class="col-lg-5 connectedSortable">
+
+              <!-- Map card -->
+              <div style="display: none;">
+                <div class="card bg-gradient-primary">
+                  <div class="card-header border-0">
+                    <h3 class="card-title">
+                      <i class="fas fa-map-marker-alt mr-1"></i>
+                      Visitors
+                    </h3>
+                    <!-- card tools -->
+                    <div class="card-tools">
+                      <button type="button" class="btn btn-primary btn-sm daterange"
+                        title="Date range">
+                        <i class="far fa-calendar-alt"></i>
+                      </button>
+                      <button type="button" class="btn btn-primary btn-sm"
+                        data-card-widget="collapse" title="Collapse">
+                        <i class="fas fa-minus"></i>
+                      </button>
+                    </div>
+                    <!-- /.card-tools -->
+                  </div>
+                  <div class="card-body">
+                    <div id="world-map" style="height: 250px; width: 100%;"></div>
+                  </div>
+                  <!-- /.card-body-->
+                  <div class="card-footer bg-transparent">
+                    <div class="row">
+                      <div class="col-4 text-center">
+                        <div id="sparkline-1"></div>
+                        <div class="text-white">Visitors</div>
+                      </div>
+                      <!-- ./col -->
+                      <div class="col-4 text-center">
+                        <div id="sparkline-2"></div>
+                        <div class="text-white">Online</div>
+                      </div>
+                      <!-- ./col -->
+                      <div class="col-4 text-center">
+                        <div id="sparkline-3"></div>
+                        <div class="text-white">Sales</div>
+                      </div>
+                      <!-- ./col -->
+                    </div>
+                    <!-- /.row -->
+                  </div>
+                </div>
+              </div>
+              <!-- /.card -->
+
+              <!-- solid sales graph -->
+              <div style="display: none;">
+                <div class="card bg-gradient-info">
+                  <div class="card-header border-0">
+                    <h3 class="card-title">
+                      <i class="fas fa-th mr-1"></i>
+                      Sales Graph
+                    </h3>
+
+                    <div class="card-tools">
+                      <button type="button" class="btn bg-info btn-sm"
+                        data-card-widget="collapse">
+                        <i class="fas fa-minus"></i>
+                      </button>
+                      <button type="button" class="btn bg-info btn-sm" data-card-widget="remove">
+                        <i class="fas fa-times"></i>
+                      </button>
+                    </div>
+                  </div>
+                  <div class="card-body">
+                    <canvas class="chart" id="line-chart"
+                      style="min-height: 250px; height: 250px; max-height: 250px; max-width: 100%;"></canvas>
+                  </div>
+                  <!-- /.card-body -->
+                  <div class="card-footer bg-transparent">
+                    <div class="row">
+                      <div class="col-4 text-center">
+                        <input type="text" class="knob" data-readonly="true" value="20"
+                          data-width="60" data-height="60" data-fgColor="#39CCCC">
+
+                        <div class="text-white">Mail-Orders</div>
+                      </div>
+                      <!-- ./col -->
+                      <div class="col-4 text-center">
+                        <input type="text" class="knob" data-readonly="true" value="50"
+                          data-width="60" data-height="60" data-fgColor="#39CCCC">
+
+                        <div class="text-white">Online</div>
+                      </div>
+                      <!-- ./col -->
+                      <div class="col-4 text-center">
+                        <input type="text" class="knob" data-readonly="true" value="30"
+                          data-width="60" data-height="60" data-fgColor="#39CCCC">
+
+                        <div class="text-white">In-Store</div>
+                      </div>
+                      <!-- ./col -->
+                    </div>
+                    <!-- /.row -->
+                  </div>
+                  <!-- /.card-footer -->
+                </div>
+              </div>
+              <!-- /.card -->
+
+           
+                <!-- /.card-body -->
+              </div>
+              <!-- /.card -->
+            </section>
+            <!-- right col -->
+          </div>
+          <!-- /.row (main row) -->
+        </div><!-- /.container-fluid -->
+      </section>
+      <!-- /.content -->
+    </div>
+    <!-- /.content-wrapper -->
+
+
+    <!-- Control Sidebar -->
+    <aside class="control-sidebar control-sidebar-dark">
+      <!-- Control sidebar content goes here -->
+    </aside>
+    <!-- /.control-sidebar -->
+  </div>
+  <!-- ./wrapper -->
+  
+  <!-- jQuery -->
+  <script src="plugins/jquery/jquery.min.js"></script>
+  <!-- jQuery UI 1.11.4 -->
+  <script src="plugins/jquery-ui/jquery-ui.min.js"></script>
+  <!-- Resolve conflict in jQuery UI tooltip with Bootstrap tooltip -->
+  <script>
+    $.widget.bridge('uibutton', $.ui.button)
+  </script>
+  <!-- Bootstrap 4 -->
+  <script src="plugins/bootstrap/js/bootstrap.bundle.min.js"></script>
+  <!-- ChartJS -->
+  <script src="plugins/chart.js/Chart.min.js"></script>
+  <!-- Sparkline -->
+  <script src="plugins/sparklines/sparkline.js"></script>
+  <!-- JQVMap -->
+  <script src="plugins/jqvmap/jquery.vmap.min.js"></script>
+  <script src="plugins/jqvmap/maps/jquery.vmap.usa.js"></script>
+  <!-- jQuery Knob Chart -->
+  <script src="plugins/jquery-knob/jquery.knob.min.js"></script>
+  <!-- daterangepicker -->
+  <script src="plugins/moment/moment.min.js"></script>
+  <script src="plugins/moment/locale/es.js"></script>
+  <script src="plugins/daterangepicker/daterangepicker.js"></script>
+  <!-- Tempusdominus Bootstrap 4 -->
+  <script src="plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js"></script>
+  <!-- Summernote -->
+  <script src="plugins/summernote/summernote-bs4.min.js"></script>
+  <!-- overlayScrollbars -->
+  <script src="plugins/overlayScrollbars/js/jquery.overlayScrollbars.min.js"></script>
+  <!-- AdminLTE App -->
+  <script src="dist/js/adminlte.js"></script>
+  <!-- AdminLTE for demo purposes -->
+  <script src="dist/js/demo.js"></script>
+  <!-- AdminLTE dashboard demo (This is only for demo purposes) -->
+  <script src="dist/js/pages/dashboard.js"></script>
+  <script>
+    $(function() {
+
+          var donutChartCanvas = $('#donutChart').get(0).getContext('2d')
+          var donutData = {
+            labels: [
+              'Sistemas Computacionales',
+              'Industrial',
+              'Logística',
+              'Gestión Empresarial',
+            ],
+            datasets: [{
+              data: [700, 500, 400, 600,],
+              backgroundColor: ['#f56954', '#00a65a', '#f39c12', '#00c0ef', '#3c8dbc', '#d2d6de'],
+            }]
+          }
+          var donutOptions = {
+            maintainAspectRatio: false,
+            responsive: true,
+          }
+          //Create pie or douhnut chart
+          // You can switch between pie and douhnut using the method below.
+          new Chart(donutChartCanvas, {
+            type: 'doughnut',
+            data: donutData,
+            options: donutOptions
+          })
+        })
+  </script>
+</body>
+
+</html>
