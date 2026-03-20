a1a31b3eab79083c745d463ce02cec60de630a2a
LuisBarajas10 <luis.barajas.22isc@tecsanpedro.edu.mx>
2026-03-20 07:46:29 -0600

    ya se realizaron los ultimos cambios pertinentes

--- portal/dashboard_alumno.php
+++ portal/dashboard_alumno.php
@@ -182,7 +182,7 @@
         <div class="container-fluid">
           <div class="row mb-2">
             <div class="col-sm-6"><br>
-              <h1 class="m-0">¡Hola, moi!👋</h1>
+              <h1 class="m-0">¡Hola, pumbitakush!👋</h1>
             </div><!-- /.col -->
 
           </div><!-- /.row -->
